5569c6af015906cc11d14aedb052ab989bc8b0b2
David Wilkening <git@david-wilkening.de>
2020-07-21 23:09:54 +0200

    Send audio instead of receiving it

--- app/src/main/java/org/rocstreaming/rocdroid/MainActivity.kt
+++ app/src/main/java/org/rocstreaming/rocdroid/MainActivity.kt
@@ -1,33 +1,56 @@
 package org.rocstreaming.rocdroid
 
-import android.media.AudioAttributes
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.*
 import android.media.AudioAttributes.USAGE_MEDIA
-import android.media.AudioFormat
-import android.media.AudioFormat.CHANNEL_OUT_STEREO
-import android.media.AudioFormat.ENCODING_PCM_FLOAT
+import android.media.AudioFormat.*
 import android.media.AudioManager.AUDIO_SESSION_ID_GENERATE
-import android.media.AudioTrack
+import android.media.AudioRecord.READ_BLOCKING
 import android.media.AudioTrack.MODE_STREAM
 import android.media.AudioTrack.PERFORMANCE_MODE_LOW_LATENCY
+import android.media.MediaRecorder.AudioSource.VOICE_PERFORMANCE
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import org.rocstreaming.roctoolkit.*
 import java.net.NetworkInterface
 
 const val SAMPLE_RATE = 44100
 const val BUFFER_SIZE = 100
 
+private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
+
 class MainActivity : AppCompatActivity() {
 
     private var thread: Thread? = null
+    // Requesting permission to RECORD_AUDIO
+    private var permissionToRecordAccepted = false
+    private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val ipTextView: TextView = findViewById(R.id.ipTextView)
         ipTextView.text = getIpAddresses()
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        } else {
+            false
+        }
+        if (!permissionToRecordAccepted) finish()
     }
 
     /**
@@ -39,37 +62,38 @@ class MainActivity : AppCompatActivity() {
         }
 
         thread = Thread(Runnable {
-            val audioTrack = createAudioTrack()
+            val audioRecord = createAudioRecord()
 
-            audioTrack.play()
-            val config = ReceiverConfig.Builder(
+            audioRecord.startRecording()
+            val config = SenderConfig.Builder(
                 SAMPLE_RATE,
                 ChannelSet.STEREO,
                 FrameEncoding.PCM_FLOAT
             ).build()
 
             Context().use { context ->
-                Receiver(context, config).use { receiver ->
-                    receiver.bind(
+                Sender(context, config).use { sender ->
+                    sender.bind(Address(Family.AUTO, "0.0.0.0", 0))
+                    sender.connect(
                         PortType.AUDIO_SOURCE,
                         Protocol.RTP_RS8M_SOURCE,
-                        Address(Family.AUTO, "0.0.0.0", 10001)
+                        Address(Family.AUTO, "100.124.83.113", 10001)
                     )
-                    receiver.bind(
+                    sender.connect(
                         PortType.AUDIO_REPAIR,
                         Protocol.RS8M_REPAIR,
-                        Address(Family.AUTO, "0.0.0.0", 10002)
+                        Address(Family.AUTO, "100.124.83.113", 10002)
                     )
 
                     val samples = FloatArray(BUFFER_SIZE)
                     while (!Thread.currentThread().isInterrupted) {
-                        receiver.read(samples)
-                        audioTrack.write(samples, 0, samples.size, AudioTrack.WRITE_BLOCKING)
+                        audioRecord.read(samples, 0, samples.size, READ_BLOCKING)
+                        sender.write(samples)
                     }
                 }
             }
 
-            audioTrack.release()
+            audioRecord.release()
         })
 
         thread!!.start()
@@ -82,7 +106,7 @@ class MainActivity : AppCompatActivity() {
         thread?.interrupt()
     }
 
-    private fun createAudioTrack(): AudioTrack {
+    private fun createAudioRecord(): AudioRecord {
         val audioAttributes = AudioAttributes.Builder()
             .setUsage(USAGE_MEDIA)
             .setFlags(PERFORMANCE_MODE_LOW_LATENCY)
@@ -90,21 +114,23 @@ class MainActivity : AppCompatActivity() {
         val audioFormat = AudioFormat.Builder()
             .setSampleRate(SAMPLE_RATE)
             .setEncoding(ENCODING_PCM_FLOAT)
-            .setChannelMask(CHANNEL_OUT_STEREO)
+            .setChannelMask(CHANNEL_OUT_STEREO) // should be mono
             .build()
 
-        val bufferSize = AudioTrack.getMinBufferSize(
+        val bufferSize = AudioRecord.getMinBufferSize(
             audioFormat.sampleRate,
-            audioFormat.encoding,
-            audioFormat.channelMask
+            audioFormat.channelMask,
+            audioFormat.encoding
         )
 
-        return AudioTrack(
-            audioAttributes,
-            audioFormat,
-            bufferSize,
-            MODE_STREAM,
-            AUDIO_SESSION_ID_GENERATE
+        return AudioRecord(
+            // https://developer.android.com/reference/kotlin/android/media/MediaRecorder.AudioSource#voice_performance
+            // unsure if choiced correctly
+            VOICE_PERFORMANCE,
+            audioFormat.sampleRate,
+            audioFormat.channelMask,
+            audioFormat.encoding,
+            bufferSize
         )
     }
 
